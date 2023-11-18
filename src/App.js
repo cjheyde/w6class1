21a8db3e87b1f8660338be806fd6e286af3b10a9
cjheyde <carlaheyde@gmail.com>
2023-11-18 21:07:35 +0100

    feat: :construction: Add a footer functional component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import FunctionalComponenet from "./FunctionalComponent";
 import Name from "./Name";
 import ClassComponent from "./ClassComponent";
 import MyClassComponent from "./MyClassComponent";
+import Footer from "./Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Name />
       <ClassComponent />
       <MyClassComponent compProp="Here is the value of the prop of the Class Component" />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+function Footer() {
+        const message = "All rights reserved to CJH - 2023"
+
+    return (
+        <footer>
+            <h3>
+                {message}
+            </h3>
+        </footer>
+    )
+}
+
+export default Footer;
