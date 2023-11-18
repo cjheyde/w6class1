d7b4adac9f70a7638118654b03880421503a7724
cjheyde <carlaheyde@gmail.com>
2023-11-19 00:47:05 +0100

    refactor: :recycle: Refactor Footer from Functional to a Class Component

--- src/Footer.js
+++ src/Footer.js
@@ -1,13 +1,15 @@
-function Footer() {
-        const message = "All rights reserved to CJH - 2023"
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    const message = "All rights reserved to CJH - 2023";
 
     return (
-        <footer>
-            <h3>
-                {message}
-            </h3>
-        </footer>
-    )
+      <footer>
+        <h3>{message}</h3>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
