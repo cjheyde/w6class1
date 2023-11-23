bc770a1ae471783ffcb14fbb9dcbd92540f950c9
Carla Jünger Heyde <carlaheyde@gmail.com>
2023-11-23 05:42:11 +0100

    refactor: :recycle: change class to className (#6)

--- src/MyClassComponent.js
+++ src/MyClassComponent.js
@@ -1,11 +1,11 @@
-// ref. https://www.digitalocean.com/community/tutorials/react-class-components 
+// ref. https://www.digitalocean.com/community/tutorials/react-class-components
 import React from "react";
 
 class MyClassComponent extends React.Component {
   render() {
     const { compProp } = this.props;
-    return <div class="myComp">{compProp}</div>;
+    return <div className="myComp">{compProp}</div>;
   }
 }
 
-export default  MyClassComponent;
+export default MyClassComponent;
